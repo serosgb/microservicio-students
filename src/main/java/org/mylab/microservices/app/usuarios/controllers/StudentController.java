0087916fae34578d2ef69501fc3713dd9fabfa7f
Sergio Gtz <sergio.oscar.gb@gmail.com>
2020-06-07 15:12:12 -0500

    Enabling Eureka Client and Updating Stundents controller

--- src/main/java/org/mylab/microservices/app/usuarios/controllers/StudentController.java
+++ src/main/java/org/mylab/microservices/app/usuarios/controllers/StudentController.java
@@ -7,6 +7,7 @@ import org.mylab.microservices.app.usuarios.models.entity.Student;
 import org.mylab.microservices.app.usuarios.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,25 +45,27 @@ public class StudentController {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(studentPersisted);
 	}
-	
-	@PutMapping("/{id")
-	public ResponseEntity<?> update(@RequestBody Student student, @PathVariable Long id) {
+
+	@PutMapping(value = "/{id", consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE })
+	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Student student) {
 		Optional<Student> optional = service.findById(id);
 
 		if (optional.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		Student studentPersisted = optional.get();
-		
+
 		studentPersisted.setName(student.getName());
 		studentPersisted.setLastname(student.getLastname());
 		studentPersisted.setEmail(student.getEmail());
 		studentPersisted.setUpdatedAt(new Date());
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(studentPersisted));
 	}
-	
+
 	@DeleteMapping("/{id")
 	public ResponseEntity<?> delete(@PathVariable Long id) {
 		service.deleteById(id);
